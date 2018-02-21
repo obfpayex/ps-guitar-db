210e0ebc619b54b38152964f3642868693a5988b
Oystein bodin fink <oystein.bodin-fink@payex.com>
2018-02-21 13:51:37 +0100

    last module

---
+++ src/main/java/com/guitar/db/repository/ModelJpaRepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.guitar.db.repository;
+
+public class ModelJpaRepositoryImpl implements ModelJpaRepositoryCustom {
+    @Override
+    public void aCustomMethode() {
+        System.out.println("I am a custom methode");
+    }
+}
