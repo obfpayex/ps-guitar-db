a6d28df7be4be78314e0cc794e10cdd46f46a569
Oystein bodin fink <oystein.bodin-fink@payex.com>
2018-02-21 12:15:01 +0100

    Next module

--- src/main/java/com/guitar/db/repository/ModelJpaRepository.java
+++ src/main/java/com/guitar/db/repository/ModelJpaRepository.java
@@ -2,6 +2,8 @@ package com.guitar.db.repository;
 
 import com.guitar.db.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -12,4 +14,9 @@ public interface ModelJpaRepository extends JpaRepository<Model,Long> {
 
     List<Model> findByPriceGreaterThanEqualAndPriceLessThanEqual(BigDecimal lowest, BigDecimal highest);
     List<Model> findByModelTypeNameIn(List<String> types);
+
+    @Query("select m from Model m where m.price >= :lowest and m.price <= :highest and m.woodType like :wood")
+    List<Model> queryByPriceRangeAndWoodType(@Param("lowest") BigDecimal lowest,@Param("highest") BigDecimal highest,@Param("wood") String wood);
+
+    List<Model> findAllModelsByType(@Param("name") String name);
 }
